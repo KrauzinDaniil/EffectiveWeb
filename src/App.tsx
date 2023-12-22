4662ff1a503e00ddaa599f2d98a7a8abbdc7a495
KrauzinDaniil <krauzin1995@gmail.com>
2023-12-23 01:08:12 +0600

    tavBar-done

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@
 import './App.css'
 import Header from './components/Header'
 import Services from './components/Services'
+import SliderPanel from './components/Slider'
 
 function App() {
   return (
         <>
        <Header/>
        <Services/>
+       <SliderPanel/>
        </>
        
       
---
+++ src/components/Services/ServiceAdditional/ServiceLeft/ServiceAdditionalLeft.tsx
@@ -0,0 +1,18 @@
+import classes from './ServiceAdditional.module.css'
+
+
+const ServiceAdditionalLeft: React.FC = () => {
+    return ( <div className= {classes.wrapper}>
+           <div> <img src = "src/assets/pictureOne.png"/></div>
+           <div className= {classes.leftTextWrapper}><h2>Leading healhcare <br /> providers</h2>
+           <div className= {classes.divider}></div>
+           <br />
+           <div className={classes.additionalInfo}><p>We provides progressive, and affordable healthcare,<br/> accessible on mobile and online for everyone. To us, it’s <br/> not just work. We take pride in the solutions we deliver</p></div>
+            <button><div>Learn more</div></button>          
+             </div>
+             
+
+
+    </div>) 
+ }
+ export default ServiceAdditionalLeft;
